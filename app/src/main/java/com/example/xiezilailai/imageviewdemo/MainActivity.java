cd997cf9951e240020c771eb7faca65952c1b73a
xiezilailai <1945926686@qq.com>
2016-04-30 15:25:49 +0800

    version 2.0

--- app/src/main/java/com/example/xiezilailai/imageviewdemo/MainActivity.java
+++ app/src/main/java/com/example/xiezilailai/imageviewdemo/MainActivity.java
@@ -14,7 +14,6 @@ import android.widget.ImageView;
 public class MainActivity extends Activity implements View.OnTouchListener {
 
 
-
     private float lastX[] = {0, 0};//用来记录上一次两个触点的横坐标
     private float lastY[] = {0, 0};//用来记录上一次两个触点的纵坐标
 
@@ -24,7 +23,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     private ImageView imageView;
 
     private static Matrix currentMatrix = new Matrix();//保存当前窗口显示的矩阵
-    private Matrix touchMatrix,mmatrix;
+    private Matrix touchMatrix, mmatrix;
     private boolean flag = false;//用来标记是否进行过移动前的首次点击
     private float moveLastX, moveLastY;//进行移动时用来记录上一个触点的坐标
 
@@ -54,7 +53,7 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     private void center() {
 
         //获取imageview中图片的实际高度
-        Bitmap bitmap=((BitmapDrawable) (imageView).getDrawable()).getBitmap();
+        Bitmap bitmap = ((BitmapDrawable) (imageView).getDrawable()).getBitmap();
         imageHeight = bitmap.getHeight();
         imageWidth = bitmap.getWidth();
 
@@ -70,9 +69,9 @@ public class MainActivity extends Activity implements View.OnTouchListener {
     public boolean onTouch(View view, MotionEvent motionEvent) {
         //注意这一句的写法，用在多点触控中
         switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
-            // TODO: 2016/4/30  在这里解释一下，在程序中我们将单点控制移动和双点控制缩放区分开（但是双点也是可以
-            // TODO: 2016/4/30  控制移动的)flag 的作用很简单，主要是用在单点移动时判断是否此次点击是否将要移动（不好描述，请读者自行细想一下）
-            // todo，否则容易与双点操作混乱在一起，给用户带来较差的用户体验
+            // 在这里解释一下，在程序中我们将单点控制移动和双点控制缩放区分开（但是双点也是可以
+            // 控制移动的)flag 的作用很简单，主要是用在单点移动时判断是否此次点击是否将要移动（不好描述，请读者自行细想一下）
+            // 否则容易与双点操作混乱在一起，给用户带来较差的用户体验
             /*
             *
             *
@@ -102,34 +101,63 @@ public class MainActivity extends Activity implements View.OnTouchListener {
                     //矩阵初始化为当前矩阵
                     touchMatrix.set(currentMatrix);
 
-                    touchMatrix.preTranslate(-(currentDistance / lastDistance - 1) * imageWidth / 2,
-                            -(currentDistance / lastDistance - 1) * imageHeight / 2);
-                    float p[]=new float[9];
+                    float pp[] = new float[9];
+                    touchMatrix.getValues(pp);
+                    float leftPosition = pp[2];//图片左边的位置
+                    float upPostion = pp[5];//图片顶部的位置
+                    /*
+                    * 缩放之前对图片进行平移，将缩放中心平移到将要缩放的位置
+                    * */
+
+                    float l = (motionEvent.getX(0) + motionEvent.getX(1)) / 2 - leftPosition;
+                    float t = (motionEvent.getY(0) + motionEvent.getY(1)) / 2 - upPostion;
+
+                    touchMatrix.postTranslate(-(currentDistance / lastDistance - 1) * l,
+                            -(currentDistance / lastDistance - 1) * t);
+                    float p[] = new float[9];
                     touchMatrix.getValues(p);
                     //根据判断当前缩放的大小来判断是否达到缩放边界
-                    if(p[0]*currentDistance / lastDistance<min_scale||p[0]*currentDistance / lastDistance>max_scale){
+                    if (p[0] * currentDistance / lastDistance < min_scale || p[0] * currentDistance / lastDistance > max_scale) {
                         //超过边界值时，设置为先前记录的矩阵
                         touchMatrix.set(mmatrix);
                         imageView.setImageMatrix(touchMatrix);
-                    }else{
+                    } else {
                         //图像缩放
                         touchMatrix.preScale(currentDistance / lastDistance, currentDistance / lastDistance);
+
                         //根据两个触点移动的距离实现位移（双触点平移）
                         float movex = (motionEvent.getX(0) - lastX[0] + motionEvent.getX(1) - lastX[1]) / 2;
                         float movey = (motionEvent.getY(0) - lastY[0] + motionEvent.getY(1) - lastY[1]) / 2;
                         touchMatrix.postTranslate(movex, movey);
                         //保存最后的矩阵，当缩放超过边界值时就设置为此矩阵
-                        mmatrix=touchMatrix;
+                        mmatrix = touchMatrix;
                         imageView.setImageMatrix(touchMatrix);
                     }
 
                 } else {
                     if (flag) {
-                        touchMatrix = new Matrix();
-                        touchMatrix.set(currentMatrix);
-                        //只有一个点时，进行位移
-                        touchMatrix.postTranslate(-moveLastX + motionEvent.getX(0), -moveLastY + motionEvent.getY(0));
-                        imageView.setImageMatrix(touchMatrix);
+
+                        //只有一个触点时进行位移
+                        Matrix tmp = new Matrix();//临时矩阵用来判断此次平移是否会导致平移越界
+                        tmp.set(currentMatrix);
+                        tmp.postTranslate(-moveLastX + motionEvent.getX(0), -moveLastY + motionEvent.getY(0));
+
+                        if (!isTranslateOver(tmp)) {
+                            //如果不越界就进行平移
+                            touchMatrix = new Matrix();
+                            touchMatrix.set(currentMatrix);
+                            touchMatrix.postTranslate(-moveLastX + motionEvent.getX(0), -moveLastY + motionEvent.getY(0));
+                            imageView.setImageMatrix(touchMatrix);
+                        } else {
+                            //如果会越界就保存当前位置，并且不进行矩阵变换
+                            currentMatrix = touchMatrix;
+                            moveLastX = motionEvent.getX(0);
+                            moveLastY = motionEvent.getY(0);
+                            imageView.setImageMatrix(touchMatrix);
+//
+                        }
+
+
                     }
 
                 }
@@ -154,6 +182,23 @@ public class MainActivity extends Activity implements View.OnTouchListener {
         return (float) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
     }
 
+    //判断平移是否越界
+    private boolean isTranslateOver(Matrix matrix) {
+        float p[] = new float[9];
+        matrix.getValues(p);
+        float leftPosition = p[2];
+        float rightPosition = (p[2] + imageWidth * p[0]);
+
+        float upPostion = p[5];
+        float downPostion = p[5] + imageHeight * p[0];
+
+        float leftSide, rightSide, upSide, downSide;
+        leftSide = windowWidth / 4;
+        rightSide = windowWidth / 4 * 3;
+        upSide = windowHeight / 4;
+        downSide = windowHeight / 4 * 3;
+        return (leftPosition > rightSide || rightPosition < leftSide || upPostion > downSide || downPostion < upSide);
+    }
 
 
 }
